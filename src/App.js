f6a7825356844c7774ff651a939c90a8a1b2e362
Yongki Satria Sanjaya <yongkisatrias@gmail.com>
2023-10-17 23:19:27 +0700

    [UPDATE] Create footer section

--- src/App.js
+++ src/App.js
@@ -194,6 +194,59 @@ function App() {
         </div>
       </section>
       {/* CTA End */}
+
+      {/* Footer Start */}
+      <footer className="container mt-10 mb-5">
+        <div className="row">
+          <div className="col-md-4 col-sm-12">
+            <div className="d-flex justify-content-center-mobile">
+              <img src="/image/logo/Tickitz 2.svg" alt="Logo Tickitz" />
+            </div>
+            <p className="tickets-footer-motto text-center-mobile">
+              Stop waiting in line. Buy tickets <br />
+              conveniently, watch movies quietly.
+            </p>
+          </div>
+          <div className="col-md-2 col-sm-12 explore text-center-mobile">
+            <h5>Explore</h5>
+            <a className="mt-4 mb-4">Home</a>
+            <a className="mt-4 mb-4">List Movie</a>
+          </div>
+          <div className="col-md-3 col-sm-12">
+            <h5 className="text-center-mobile">Our Sponsor</h5>
+            <div className="d-flex justify-content-center-mobile">
+              <img src="/image/cinemas/ebv.id 2.svg" alt="" srcset="" style={{ display: "block" }} className="mt-4 mb-4" />
+            </div>
+            <div className="d-flex justify-content-center-mobile">
+              <img src="/image/cinemas/CineOne21 2.svg" alt="" srcset="" style={{ display: "block" }} className="mt-4 mb-4" />
+            </div>
+            <div className="d-flex justify-content-center-mobile">
+              <img src="/image/cinemas/hiflix 2.svg" alt="" srcset="" style={{ display: "block" }} className="mt-4 mb-4" />
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-12 social-media">
+            <h5 className="text-center-mobile">Follow Us</h5>
+            <div className="d-flex justify-content-center-mobile gap-3 mt-4 mb-4">
+              <img src="/image/social-media/facebook.png" alt="" srcset="" />
+              <a href="#">Tickitz Cinema id</a>
+            </div>
+            <div className="d-flex justify-content-center-mobile gap-3 mt-4 mb-4">
+              <img src="/image/social-media/instagram.png" alt="" srcset="" />
+              <a href="#">tickitz.id</a>
+            </div>
+            <div className="d-flex justify-content-center-mobile gap-3 mt-4 mb-4">
+              <img src="/image/social-media/twitter.png" alt="" srcset="" />
+              <a href="#">tickitz.id</a>
+            </div>
+            <div className="d-flex justify-content-center-mobile gap-3 mt-4 mb-4">
+              <img src="/image/social-media/youtube.png" alt="" srcset="" />
+              <a href="#">Tickitz Cinema id</a>
+            </div>
+          </div>
+        </div>
+        <p className="copyright text-center mt-5">© 2020 Tickitz. All Rights Reserved.</p>
+      </footer>
+      {/* Footer End */}
     </div>
   );
 }
